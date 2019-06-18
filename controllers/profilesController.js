e393458a75105980ce2df3b88ad81548ee674339
Alexander Bryksin <alexander.bryksin@yandex.ru>
2019-06-18 13:51:46 +0300

    Server Profile controller refactor to separate create and update handlers

--- controllers/profilesController.js
+++ controllers/profilesController.js
@@ -2,6 +2,7 @@ const Profile = require('../models/Profile')
 const User = require('../models/User')
 const { validationResult } = require('express-validator/check')
 
+// @ GET Get current user profile
 module.exports.getCurrentUserProfile = async (req, res) => {
   try {
     // find user profile by user.id and populate user object fields
@@ -15,7 +16,8 @@ module.exports.getCurrentUserProfile = async (req, res) => {
   }
 }
 
-module.exports.createOrUpdateUserProfile = async (req, res) => {
+// @ POST Create user profile
+module.exports.createUserProfile = async (req, res) => {
   const {
     company,
     website,
@@ -60,14 +62,8 @@ module.exports.createOrUpdateUserProfile = async (req, res) => {
     // search user profile by user.id
     let profile = await Profile.findOne({ user: req.user.id })
 
-    // if profile exists - update
-    if (profile) {
-      // update find by user.id, $set: update user object, new: true - for get updated user back
-      profile = await Profile.findOneAndUpdate({ user: req.user.id }, { $set: profileFields }, { new: true })
-
-      console.log('PROFILE UDPATE => ', profile)
-      return res.status(201).json(profile)
-    }
+    // if profile  exists - error
+    if (profile) return res.status(400).json({ errors: [{ msg: 'Profile already exists' }] })
 
     // Create profile
     profile = new Profile(profileFields)
@@ -75,6 +71,66 @@ module.exports.createOrUpdateUserProfile = async (req, res) => {
 
     console.log('PROFILE CREATE => ', profile)
     res.status(201).json(profile)
+  } catch (error) {
+    console.error(error)
+    res.status(500).send('Server Error')
+  }
+}
+
+// @ PATCH Update user profile
+module.exports.updateUserProfile = async (req, res) => {
+  const {
+    company,
+    website,
+    location,
+    bio,
+    status,
+    githubusername,
+    skills,
+    youtube,
+    facebook,
+    twitter,
+    instagram,
+    linkedin
+  } = req.body
+
+  // Build profile object
+  const profileFields = {}
+  profileFields.user = req.user.id
+  if (company) profileFields.company = company
+  if (website) profileFields.website = website
+  if (location) profileFields.location = location
+  if (bio) profileFields.bio = bio
+  if (status) profileFields.status = status
+  if (githubusername) profileFields.githubusername = githubusername
+  if (skills) {
+    profileFields.skills = skills.split(',').map((skill) => skill.trim())
+  }
+
+  // Build social object
+  profileFields.social = {}
+  if (youtube) profileFields.social.youtube = youtube
+  if (twitter) profileFields.social.twitter = twitter
+  if (facebook) profileFields.social.facebook = facebook
+  if (linkedin) profileFields.social.linkedin = linkedin
+  if (instagram) profileFields.social.instagram = instagram
+
+  try {
+    // validate
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array() })
+
+    // search user profile by user.id
+    let profile = await Profile.findOne({ user: req.user.id })
+
+    // if profile does not exists - update
+    if (!profile) return res.status(400).json({ errors: [{ msg: 'Not found profile for this user' }] })
+
+    // update find by user.id, $set: update user object, new: true - for get updated user back
+    profile = await Profile.findOneAndUpdate({ user: req.user.id }, { $set: profileFields }, { new: true })
+
+    console.log('PROFILE UDPATE => ', profile)
+    return res.status(201).json(profile)
   } catch (error) {
     console.error(error)
     res.status(500).send('Server Error')
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -7,14 +7,38 @@ const { check } = require('express-validator/check')
 // @ GET Private api/v1/profile Get Current user profile
 router.get('/me', authMiddleware, ProfilesController.getCurrentUserProfile)
 
-// @ GET Private api/v1/profile Create or Update user profile
-router.post('/', [authMiddleware, [
-  check('status', 'Status is required')
-    .not()
-    .isEmpty(),
-  check('skills', 'Skills is required')
-    .not()
-    .isEmpty()
-]], ProfilesController.createOrUpdateUserProfile)
+// @ POST Private api/v1/profile Create or Update user profile
+router.post(
+  '/',
+  [
+    authMiddleware,
+    [
+      check('status', 'Status is required')
+        .not()
+        .isEmpty(),
+      check('skills', 'Skills is required')
+        .not()
+        .isEmpty()
+    ]
+  ],
+  ProfilesController.createUserProfile
+)
+
+// @ PATCH Private api/v1/profile Create or Update user profile
+router.patch(
+  '/',
+  [
+    authMiddleware,
+    [
+      check('status', 'Status is required')
+        .not()
+        .isEmpty(),
+      check('skills', 'Skills is required')
+        .not()
+        .isEmpty()
+    ]
+  ],
+  ProfilesController.updateUserProfile
+)
 
 module.exports = router
