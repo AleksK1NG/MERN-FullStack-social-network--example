340824481f24568089fbfc8b545c82cf4675eb55
Alexander Bryksin <alexander.bryksin@yandex.ru>
2019-06-18 19:40:52 +0300

    Server Post controller create post handler

---
+++ controllers/postsController.js
@@ -0,0 +1,32 @@
+const Profile = require('../models/Profile')
+const User = require('../models/User')
+const Post = require('../models/Post')
+const { validationResult } = require('express-validator/check')
+const config = require('config')
+const request = require('request')
+
+// @ POST Create post
+module.exports.createPost = async (req, res) => {
+  try {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() })
+    }
+
+    const user = await User.findById(req.user.id).select('-password')
+
+    const newPost = new Post({
+      text: req.body.text,
+      name: user.name,
+      avatar: user.avatar,
+      user: req.user.id
+    })
+
+    const post = await newPost.save()
+
+    res.status(201).json(post)
+  } catch (error) {
+    console.error(error.message)
+    res.status(500).send('Server Error')
+  }
+}
--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,9 +1,19 @@
 const express = require('express')
 const router = express.Router()
+const authMiddleware = require('../../middlewares/authMiddleware')
+const PostsController = require('../../controllers/postsController')
+const { check } = require('express-validator/check')
 
 // @ GET Public api/v1/posts
-router.get('/', async (req, res) => {
-  res.send('Success =D')
-})
+router.post(
+  '/',
+  [
+    authMiddleware,
+    check('text', 'Text is required')
+      .not()
+      .isEmpty()
+  ],
+  PostsController.createPost
+)
 
 module.exports = router
