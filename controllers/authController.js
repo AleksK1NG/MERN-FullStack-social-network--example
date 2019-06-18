ea7c9086abbe70e03bd5401549a34a49dae5c1ef
Alexander Bryksin <alexander.bryksin@yandex.ru>
2019-06-18 12:53:12 +0300

    Server Auth controller complete load, login, register

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,6 @@
 const User = require('../models/User')
 const { validationResult } = require('express-validator/check')
+const gravatar = require('gravatar')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const config = require('config')
@@ -33,6 +34,40 @@ module.exports.authAndGetToken = async (req, res) => {
     if (!isMatch) return res.status(400).json({ errors: [{ msg: 'Invalid Credentials' }] })
 
     const payload = { user: { id: user.id } }
+
+    // Add token to user
+    await jwt.sign(payload, config.get('JWT_SECRET'), { expiresIn: 360000 }, (error, token) => {
+      if (error) throw error
+      user.token = token
+      res.status(201).json(user.toAuthJSON())
+    })
+  } catch (error) {
+    console.error(error)
+    res.status(500).send('Server Error')
+  }
+}
+
+module.exports.register = async (req, res) => {
+  const { email, name, password, info } = req.body
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() })
+  }
+  try {
+    // check user exists
+    let user = await User.findOne({ email })
+    if (user) return res.status(400).json({ errors: [{ msg: 'User already exists' }] })
+    // create avatar
+    const avatar = gravatar.url(email, { s: '200', r: 'pg', d: 'mm' })
+    // create user
+    user = new User({ email, name, info, avatar })
+    // create hashed password
+    const salt = await bcrypt.genSalt(10)
+    user.password = await bcrypt.hash(password, salt)
+    const payload = { user: { id: user.id } }
+
+    // save user in db
+    await user.save()
 
     // Add token to user
     await jwt.sign(payload, config.get('JWT_SECRET'), { expiresIn: 360000 }, (error, token) => {
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -4,13 +4,27 @@ const authMiddleware = require('../../middlewares/authMiddleware')
 const AuthController = require('../../controllers/authController')
 const { check } = require('express-validator/check')
 
-// @ GET Public api/v1/auth
+// @ GET Public api/v1/auth Load current user by token
 router.get('/', authMiddleware, AuthController.loadUser)
 
-// @ POST Public api/v1/users
-router.post('/',[
-  check('email', 'Please enter valid email').isEmail(),
-  check('password', 'Password is required').exists()
-], AuthController.authAndGetToken)
+// @ POST Public api/v1/auth Login user
+router.post(
+  '/login',
+  [check('email', 'Please enter valid email').isEmail(), check('password', 'Password is required').exists()],
+  AuthController.authAndGetToken
+)
+
+// @ POST Public api/v1/auth Register user
+router.post(
+  '/register',
+  [
+    check('name', 'Name is required')
+      .not()
+      .isEmpty(),
+    check('email', 'Please enter valid email').isEmail(),
+    check('password', 'Please enter password with 6 or more length').isLength({ min: 6 })
+  ],
+  AuthController.register
+)
 
 module.exports = router
