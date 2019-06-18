178ce2e6662db252a0508da34391c235f40a0175
Alexander Bryksin <alexander.bryksin@yandex.ru>
2019-06-18 13:41:02 +0300

    Server Profile controller create and update handler

--- controllers/profilesController.js
+++ controllers/profilesController.js
@@ -1,5 +1,6 @@
 const Profile = require('../models/Profile')
 const User = require('../models/User')
+const { validationResult } = require('express-validator/check')
 
 module.exports.getCurrentUserProfile = async (req, res) => {
   try {
@@ -10,6 +11,72 @@ module.exports.getCurrentUserProfile = async (req, res) => {
     res.status(200).json(profile)
   } catch (error) {
     console.error(error.message)
+    res.status(500).send('Server Error')
+  }
+}
+
+module.exports.createOrUpdateUserProfile = async (req, res) => {
+  const {
+    company,
+    website,
+    location,
+    bio,
+    status,
+    githubusername,
+    skills,
+    youtube,
+    facebook,
+    twitter,
+    instagram,
+    linkedin
+  } = req.body
+
+  // Build profile object
+  const profileFields = {}
+  profileFields.user = req.user.id
+  if (company) profileFields.company = company
+  if (website) profileFields.website = website
+  if (location) profileFields.location = location
+  if (bio) profileFields.bio = bio
+  if (status) profileFields.status = status
+  if (githubusername) profileFields.githubusername = githubusername
+  if (skills) {
+    profileFields.skills = skills.split(',').map((skill) => skill.trim())
+  }
+
+  // Build social object
+  profileFields.social = {}
+  if (youtube) profileFields.social.youtube = youtube
+  if (twitter) profileFields.social.twitter = twitter
+  if (facebook) profileFields.social.facebook = facebook
+  if (linkedin) profileFields.social.linkedin = linkedin
+  if (instagram) profileFields.social.instagram = instagram
+
+  try {
+    // validate
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array() })
+
+    // search user profile by user.id
+    let profile = await Profile.findOne({ user: req.user.id })
+
+    // if profile exists - update
+    if (profile) {
+      // update find by user.id, $set: update user object, new: true - for get updated user back
+      profile = await Profile.findOneAndUpdate({ user: req.user.id }, { $set: profileFields }, { new: true })
+
+      console.log('PROFILE UDPATE => ', profile)
+      return res.status(201).json(profile)
+    }
+
+    // Create profile
+    profile = new Profile(profileFields)
+    await profile.save()
+
+    console.log('PROFILE CREATE => ', profile)
+    res.status(201).json(profile)
+  } catch (error) {
+    console.error(error)
     res.status(500).send('Server Error')
   }
 }
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -7,4 +7,14 @@ const { check } = require('express-validator/check')
 // @ GET Private api/v1/profile Get Current user profile
 router.get('/me', authMiddleware, ProfilesController.getCurrentUserProfile)
 
+// @ GET Private api/v1/profile Create or Update user profile
+router.post('/', [authMiddleware, [
+  check('status', 'Status is required')
+    .not()
+    .isEmpty(),
+  check('skills', 'Skills is required')
+    .not()
+    .isEmpty()
+]], ProfilesController.createOrUpdateUserProfile)
+
 module.exports = router
