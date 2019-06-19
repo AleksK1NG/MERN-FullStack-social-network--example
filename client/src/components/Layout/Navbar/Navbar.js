25cbe4730dcedf3e8c2ef872cbd966c581df00b2
Alexander Bryksin <alexander.bryksin@yandex.ru>
2019-06-19 14:24:27 +0300

    Client Navbar auth links

--- client/src/components/Layout/Navbar/Navbar.js
+++ client/src/components/Layout/Navbar/Navbar.js
@@ -1,28 +1,62 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import './Navbar.scss'
 import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { isAuthenticatedSelector, isLoadingSelector } from '../../../ducks/auth/authSelectors'
+import { logoutUser } from '../../../ducks/auth/authActions'
 
-const Navbar = () => {
+const Navbar = ({ isAuthenticated, isLoading, logoutUser }) => {
+  const authLinks = (
+    <ul>
+      <li>
+        <Link to="/profiles">Users</Link>
+      </li>
+      <li>
+        <Link to="/posts">Posts</Link>
+      </li>
+      <li>
+        <Link to="/dashboard">
+          <i className="fas fa-user" /> <span className="hide-sm">Dashboard</span>
+        </Link>
+      </li>
+      <li>
+        <a onClick={logoutUser} href="#!">
+          <i className="fas fa-sign-out-alt" /> <span className="hide-sm">Logout</span>
+        </a>
+      </li>
+    </ul>
+  )
+
+  const guestLinks = (
+    <ul>
+      <li>
+        <Link to="/profiles">Users</Link>
+      </li>
+      <li>
+        <Link to="/register">Register</Link>
+      </li>
+      <li>
+        <Link to="/login">Login</Link>
+      </li>
+    </ul>
+  )
+
   return (
     <nav className="navbar bg-dark">
       <h1>
         <Link to="/">
-          <i className="fas fa-code"></i> Alexander Bryksin
+          <i className="fas fa-code" /> Alexander Bryksin
         </Link>
       </h1>
-      <ul>
-        <li>
-          <Link to="/">Users</Link>
-        </li>
-        <li>
-          <Link to="/register">Register</Link>
-        </li>
-        <li>
-          <Link to="/login">Login</Link>
-        </li>
-      </ul>
+      {!isLoading && <Fragment>{isAuthenticated ? authLinks : guestLinks}</Fragment>}
     </nav>
   )
 }
 
-export default Navbar
+export default connect(
+  (state) => ({
+    isAuthenticated: isAuthenticatedSelector(state),
+    isLoading: isLoadingSelector(state)
+  }),
+  { logoutUser }
+)(Navbar)
