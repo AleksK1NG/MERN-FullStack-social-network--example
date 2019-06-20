cfd2e9dbf01c695c30e712bed55a15e74130b47b
Alexander Bryksin <alexander.bryksin@yandex.ru>
2019-06-20 11:23:48 +0300

    Client CreateProfilePage init final form

--- client/src/pages/CreateProfilePage/CreateProfilePage.js
+++ client/src/pages/CreateProfilePage/CreateProfilePage.js
@@ -1,7 +1,28 @@
 import React from 'react'
+import { Field, Form } from 'react-final-form'
 import './CreateProfilePage.scss'
 
+const categories = [
+  'Developer',
+  'Junior Developer',
+  'Senior Developer',
+  'Manager',
+  'Student or Learning',
+  'Instructor or Teacher',
+  'Intern',
+  'CEO',
+  'CTO',
+  'Other'
+]
+
 const CreateProfilePage = () => {
+  const onSubmit = (values, formApi) => {
+    console.log('Create Profile form =>', values)
+    // registerUser(newUser)
+
+    // formApi.reset()
+  }
+
   return (
     <section className="container">
       <h1 className="large text-primary">Create Your Profile</h1>
@@ -9,82 +30,129 @@ const CreateProfilePage = () => {
         <i className="fas fa-user"></i> Let's get some information to make your profile stand out
       </p>
       <small>* = required field</small>
-      <form className="form">
-        <div className="form-group">
-          <select name="status">
-            <option value="0">* Select Professional Status</option>
-            <option value="Developer">Developer</option>
-            <option value="Junior Developer">Junior Developer</option>
-            <option value="Senior Developer">Senior Developer</option>
-            <option value="Manager">Manager</option>
-            <option value="Student or Learning">Student or Learning</option>
-            <option value="Instructor">Instructor or Teacher</option>
-            <option value="Intern">Intern</option>
-            <option value="Other">Other</option>
-          </select>
-          <small className="form-text">Give us an idea of where you are at in your career</small>
-        </div>
-        <div className="form-group">
-          <input type="text" placeholder="Company" name="company" />
-          <small className="form-text">Could be your own company or one you work for</small>
-        </div>
-        <div className="form-group">
-          <input type="text" placeholder="Website" name="website" />
-          <small className="form-text">Could be your own or a company website</small>
-        </div>
-        <div className="form-group">
-          <input type="text" placeholder="Location" name="location" />
-          <small className="form-text">City & state suggested (eg. Boston, MA)</small>
-        </div>
-        <div className="form-group">
-          <input type="text" placeholder="* Skills" name="skills" />
-          <small className="form-text">Please use comma separated values (eg. HTML,CSS,JavaScript,PHP)</small>
-        </div>
-        <div className="form-group">
-          <input type="text" placeholder="Github Username" name="githubusername" />
-          <small className="form-text">If you want your latest repos and a Github link, include your username</small>
-        </div>
-        <div className="form-group">
-          <textarea placeholder="A short bio of yourself" name="bio"></textarea>
-          <small className="form-text">Tell us a little about yourself</small>
-        </div>
 
-        <div className="my-2">
-          <button type="button" className="btn btn-light">
-            Add Social Network Links
-          </button>
-          <span>Optional</span>
-        </div>
+      <Form
+        onSubmit={onSubmit}
+        render={({ handleSubmit, pristine, invalid }) => (
+          <form onSubmit={handleSubmit} className="form">
+            <Field name="status" component="select" type="text" label="Categories">
+              {({ input, meta }) => (
+                <div className="form-group">
+                  <select {...input}>
+                    <option value="category">* Select Professional Status</option>
+                    {categories &&
+                      categories.map((category, id) => (
+                        <option value={category} key={id}>
+                          {category}
+                        </option>
+                      ))}
+                  </select>
+                  <small className="form-text">Give us an idea of where you are at in your career</small>
+                  {meta.touched && meta.error && <span className="help is-danger">{meta.error}</span>}
+                </div>
+              )}
+            </Field>
 
-        <div className="form-group social-input">
-          <i className="fab fa-twitter fa-2x"></i>
-          <input type="text" placeholder="Twitter URL" name="twitter" />
-        </div>
+            <Field name="company" component="input" type="text" label="Company">
+              {({ input, meta }) => (
+                <div className="form-group">
+                  <input type="text" {...input} placeholder="Company" />
+                  <small className="form-text">Could be your own company or one you work for</small>
+                  {meta.touched && meta.error && <span className="help is-danger">{meta.error}</span>}
+                </div>
+              )}
+            </Field>
 
-        <div className="form-group social-input">
-          <i className="fab fa-facebook fa-2x"></i>
-          <input type="text" placeholder="Facebook URL" name="facebook" />
-        </div>
+            <Field name="website" component="input" type="text" label="Website">
+              {({ input, meta }) => (
+                <div className="form-group">
+                  <input type="text" {...input} placeholder="Website" />
+                  <small className="form-text">Could be your own or a company website</small>
+                  {meta.touched && meta.error && <span className="help is-danger">{meta.error}</span>}
+                </div>
+              )}
+            </Field>
 
-        <div className="form-group social-input">
-          <i className="fab fa-youtube fa-2x"></i>
-          <input type="text" placeholder="YouTube URL" name="youtube" />
-        </div>
+            <Field name="location" component="input" type="text" label="Location">
+              {({ input, meta }) => (
+                <div className="form-group">
+                  <input type="text" {...input} placeholder="Location" />
+                  <small className="form-text">City & state suggested (eg. Boston, MA)</small>
+                  {meta.touched && meta.error && <span className="help is-danger">{meta.error}</span>}
+                </div>
+              )}
+            </Field>
 
-        <div className="form-group social-input">
-          <i className="fab fa-linkedin fa-2x"></i>
-          <input type="text" placeholder="Linkedin URL" name="linkedin" />
-        </div>
+            <Field name="skills" component="input" type="text" label="* Skills">
+              {({ input, meta }) => (
+                <div className="form-group">
+                  <input type="text" {...input} placeholder="* Skills" />
+                  <small className="form-text">Please use comma separated values (eg. HTML,CSS,JavaScript,PHP)</small>
+                  {meta.touched && meta.error && <span className="help is-danger">{meta.error}</span>}
+                </div>
+              )}
+            </Field>
 
-        <div className="form-group social-input">
-          <i className="fab fa-instagram fa-2x"></i>
-          <input type="text" placeholder="Instagram URL" name="instagram" />
-        </div>
-        <input type="submit" className="btn btn-primary my-1" />
-        <a className="btn btn-light my-1" href="dashboard.html">
-          Go Back
-        </a>
-      </form>
+            <Field name="githubusername" component="input" type="text" label="Github Username">
+              {({ input, meta }) => (
+                <div className="form-group">
+                  <input type="text" {...input} placeholder="Github Username" />
+                  <small className="form-text">
+                    If you want your latest repos and a Github link, include your username
+                  </small>
+                  {meta.touched && meta.error && <span className="help is-danger">{meta.error}</span>}
+                </div>
+              )}
+            </Field>
+
+            <Field name="bio" component="textarea" type="text" label="A short bio of yourself">
+              {({ input, meta }) => (
+                <div className="field">
+                  <textarea {...input} className="textarea" placeholder="A short bio of yourself" rows="3" />
+                  <small className="form-text">Tell us a little about yourself</small>
+                  {meta.touched && meta.error && <span className="help is-danger">{meta.error}</span>}
+                </div>
+              )}
+            </Field>
+
+            <div className="my-2">
+              <button type="button" className="btn btn-light">
+                Add Social Network Links
+              </button>
+              <span>Optional</span>
+            </div>
+
+            <div className="form-group social-input">
+              <i className="fab fa-twitter fa-2x"></i>
+              <input type="text" placeholder="Twitter URL" name="twitter" />
+            </div>
+
+            <div className="form-group social-input">
+              <i className="fab fa-facebook fa-2x"></i>
+              <input type="text" placeholder="Facebook URL" name="facebook" />
+            </div>
+
+            <div className="form-group social-input">
+              <i className="fab fa-youtube fa-2x"></i>
+              <input type="text" placeholder="YouTube URL" name="youtube" />
+            </div>
+
+            <div className="form-group social-input">
+              <i className="fab fa-linkedin fa-2x"></i>
+              <input type="text" placeholder="Linkedin URL" name="linkedin" />
+            </div>
+
+            <div className="form-group social-input">
+              <i className="fab fa-instagram fa-2x"></i>
+              <input type="text" placeholder="Instagram URL" name="instagram" />
+            </div>
+            <input type="submit" className="btn btn-primary my-1" />
+            <a className="btn btn-light my-1" href="dashboard.html">
+              Go Back
+            </a>
+          </form>
+        )}
+      />
     </section>
   )
 }
