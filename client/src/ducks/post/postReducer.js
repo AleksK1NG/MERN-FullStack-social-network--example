bbd2ccd0425227f2d14dfb2ce71f6c26a147b0a2
Alexander Bryksin <alexander.bryksin@yandex.ru>
2019-06-23 11:46:02 +0300

    Client Post module post crud sagas

--- client/src/ducks/post/postReducer.js
+++ client/src/ducks/post/postReducer.js
@@ -1,5 +1,16 @@
 import { fromJS } from 'immutable'
-import { GET_POSTS_REQUEST, GET_POSTS_SUCCESS } from './postConstants'
+import {
+  CREATE_POST_ERROR,
+  CREATE_POST_SUCCESS,
+  DELETE_POST_ERROR,
+  DELETE_POST_SUCCESS,
+  GET_POST_BY_ID_ERROR,
+  GET_POST_BY_ID_SUCCESS,
+  GET_POSTS_ERROR,
+  GET_POSTS_REQUEST,
+  GET_POSTS_SUCCESS
+} from './postConstants'
+import { GET_PROFILE_BY_ID_ERROR } from '../profile/profileConstants'
 
 /**
  * Reducer
@@ -23,6 +34,30 @@ export default function reducer(state = ReducerRecord, action) {
         .set('posts', fromJS(payload.data))
         .set('isLoading', false)
         .set('error', null)
+
+    case GET_POST_BY_ID_SUCCESS:
+      return state
+        .set('post', fromJS(payload.data))
+        .set('isLoading', false)
+        .set('error', null)
+
+    case CREATE_POST_SUCCESS:
+      return state
+        .update('posts', (posts) => posts.unshift(fromJS(payload.data)))
+        .set('isLoading', false)
+        .set('error', null)
+
+    case DELETE_POST_SUCCESS:
+      return state
+        .update('posts', (posts) => posts.filter((p) => p.get('_id') !== payload.postId))
+        .set('isLoading', false)
+        .set('error', null)
+
+    case GET_POST_BY_ID_ERROR:
+    case GET_POSTS_ERROR:
+    case CREATE_POST_ERROR:
+    case DELETE_POST_ERROR:
+      return state.set('error', payload.error).set('isLoading', false)
 
     default:
       return state
--- client/src/ducks/post/postSaga.js
+++ client/src/ducks/post/postSaga.js
@@ -5,7 +5,17 @@ import { takeEvery, call, put, all } from 'redux-saga/effects'
 import { rejectError } from '../../utils/rejectErrorHelper'
 import api from '../../services/api'
 
-import { GET_POSTS_ERROR, GET_POSTS_REQUEST, GET_POSTS_SUCCESS } from './postConstants'
+import {
+  CREATE_POST_ERROR,
+  CREATE_POST_REQUEST,
+  CREATE_POST_SUCCESS, DELETE_POST_ERROR, DELETE_POST_REQUEST, DELETE_POST_SUCCESS,
+  GET_POST_BY_ID_REQUEST,
+  GET_POSTS_ERROR,
+  GET_POSTS_REQUEST,
+  GET_POSTS_SUCCESS
+} from './postConstants'
+import { GET_PROFILE_BY_ID_ERROR, GET_PROFILE_BY_ID_SUCCESS } from '../profile/profileConstants'
+import { replace } from "connected-react-router"
 
 /**
  * Sagas
@@ -13,7 +23,7 @@ import { GET_POSTS_ERROR, GET_POSTS_REQUEST, GET_POSTS_SUCCESS } from './postCon
 
 export function* getPostsSaga() {
   try {
-    const { data } = yield call(api.registerUser)
+    const { data } = yield call(api.getAllPosts)
 
     yield put({
       type: GET_POSTS_SUCCESS,
@@ -30,6 +40,76 @@ export function* getPostsSaga() {
   }
 }
 
+export function* getPostByIdSaga({ payload }) {
+  try {
+    const { data } = yield call(api.getPostById, payload.postId)
+
+    yield put({
+      type: GET_PROFILE_BY_ID_SUCCESS,
+      payload: { data }
+    })
+    debugger
+  } catch (error) {
+    console.log(error)
+    yield put({
+      type: GET_PROFILE_BY_ID_ERROR,
+      payload: { error }
+    })
+    toast.error(rejectError(error))
+  }
+}
+
+export function* createPostSaga({ payload }) {
+  try {
+    const { data } = yield call(api.createPost, payload.postData)
+
+    yield put({
+      type: CREATE_POST_SUCCESS,
+      payload: { data }
+    })
+    debugger
+
+    toast.success('Post created ! =D')
+    // yield put(replace('/dashboard'))
+  } catch (error) {
+    console.log(error)
+    yield put({
+      type: CREATE_POST_ERROR,
+      payload: { error }
+    })
+    toast.error(rejectError(error))
+  }
+}
+
+export function* deletePostSaga({ payload }) {
+  try {
+    const { data } = yield call(api.deletePost, payload.postId)
+
+    yield put({
+      type: DELETE_POST_SUCCESS,
+      payload: { data, postId: payload.postId }
+    })
+    debugger
+
+    toast.success('Post Deleted ! =D')
+    // yield put(replace('/dashboard'))
+  } catch (error) {
+    console.log(error)
+    yield put({
+      type: DELETE_POST_ERROR,
+      payload: { error }
+    })
+    toast.error(rejectError(error))
+  }
+}
+
+
+
 export function* saga() {
-  yield all([takeEvery(GET_POSTS_REQUEST, getPostsSaga)])
+  yield all([
+    takeEvery(GET_POSTS_REQUEST, getPostsSaga),
+    takeEvery(GET_POST_BY_ID_REQUEST, getPostByIdSaga),
+    takeEvery(CREATE_POST_REQUEST, createPostSaga),
+    takeEvery(DELETE_POST_REQUEST, deletePostSaga),
+  ])
 }
